7917c5c74627455e9d57036f5ba335241630e65a
Ilya Tsibul <tsigel@vikivostok.ru>
2023-01-16 09:21:46 +0300

    tg_bot added

---
+++ tg_bot/tg_bot.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+# This is a simple echo bot using the decorator mechanism.
+# It echoes any incoming text messages.
+# https://t.me/Mafia_upsys_bot
+
+import telebot
+from config import API
+
+API_TOKEN = API
+
+bot = telebot.TeleBot(API_TOKEN)
+
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am EchoBot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
