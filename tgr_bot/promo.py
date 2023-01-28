bebb7cc4ddd5b01747fb4234fde3e72ea5959866
Ilya Tsibul <tsigel@vikivostok.ru>
2023-01-28 09:59:09 +0300

    for main text, for prices, for discounts

---
+++ tgr_bot/promo.py
@@ -0,0 +1,28 @@
+from game_management.models import Promo
+
+def promo_text():
+    promo = Promo.objects.filter(active=True, promo_type='О нас').order_by('-promo_date').first()
+    promo_txt = '\t' + promo.par1 + '\t' + promo.text1 + '\n\n' + \
+                '\t' + promo.par2 + '\t' + promo.text2 + '\n\n' + \
+                '\t' + promo.par3 + '\t' + promo.text3 + '\n\n' + \
+                '\t' + promo.par4 + '\t' + promo.text4
+    return promo_txt
+
+
+def price_text():
+    promo = Promo.objects.filter(active=True, promo_type='Цены').order_by('-promo_date').first()
+    price_txt = '\t' + promo.par1 + '\t' + promo.text1 + '\n\n' + \
+                '\t' + promo.par2 + '\t' + promo.text2 + '\n\n' + \
+                '\t' + promo.par3 + '\t' + promo.text3 + '\n\n' + \
+                '\t' + promo.par4 + '\t' + promo.text4
+    return price_txt
+
+
+def discount_text():
+    promo = Promo.objects.filter(active=True, promo_type='Скидки').order_by('-promo_date').first()
+    discount_txt = '\t' + promo.par1 + '\t' + promo.text1 + '\n\n' + \
+                '\t' + promo.par2 + '\t' + promo.text2 + '\n\n' + \
+                '\t' + promo.par3 + '\t' + promo.text3 + '\n\n' + \
+                '\t' + promo.par4 + '\t' + promo.text4
+
+    return discount_txt
