605cd229072af3faeaaeb1f5ccb415237f1be8ed
Ilya Tsibul <tsigel@vikivostok.ru>
2023-01-28 21:07:16 +0300

    Added member & master rating

--- tgr_bot/promo.py
+++ tgr_bot/promo.py
@@ -26,3 +26,23 @@ def discount_text():
                 '\t' + promo.par4 + '\t' + promo.text4
 
     return discount_txt
+
+
+def member_rating():
+    promo = Promo.objects.filter(active=True, promo_type='Рейтинг игроков').order_by('-promo_date').first()
+    member_txt = '\t' + promo.par1 + '\t' + promo.text1 + '\n\n' + \
+                '\t' + promo.par2 + '\t' + promo.text2 + '\n\n' + \
+                '\t' + promo.par3 + '\t' + promo.text3 + '\n\n' + \
+                '\t' + promo.par4 + '\t' + promo.text4
+
+    return member_txt
+
+
+def master_rating():
+    promo = Promo.objects.filter(active=True, promo_type='Рейтинг ведущих').order_by('-promo_date').first()
+    master_txt = '\t' + promo.par1 + '\t' + promo.text1 + '\n\n' + \
+                '\t' + promo.par2 + '\t' + promo.text2 + '\n\n' + \
+                '\t' + promo.par3 + '\t' + promo.text3 + '\n\n' + \
+                '\t' + promo.par4 + '\t' + promo.text4
+
+    return master_txt
