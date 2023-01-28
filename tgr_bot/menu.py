fe58d9fceb98d0a7471718377b668d214f74cfd3
Ilya Tsibul <tsigel@vikivostok.ru>
2023-01-28 21:08:44 +0300

    button sets for different users

--- tgr_bot/menu.py
+++ tgr_bot/menu.py
@@ -2,7 +2,17 @@ import telebot
 from telebot import types
 
 
-def main_buttons():
+def show_buttons(type):
+    if type == 'игрок':
+        return player_buttons()
+    elif type =='гость':
+        return guest_buttons()
+    elif type =='ведущий':
+        return master_buttons()
+
+
+
+def player_buttons():
     markup = types.ReplyKeyboardMarkup(row_width=3)
     about_us = types.KeyboardButton('О нас')
     call_back = types.KeyboardButton('Обратный звонок')
@@ -17,5 +27,39 @@ def main_buttons():
     masters_rating = types.KeyboardButton('Рейтинг ведущих')
     members_rating = types.KeyboardButton('Рейтинг игроков')
     markup.add(about_us, call_back, order_event, schedule, prices, discount, who_registered, to_register,
+               decline_register, vote_masters, masters_rating, members_rating)
+    return markup
+
+def guest_buttons():
+    markup = types.ReplyKeyboardMarkup(row_width=3)
+    about_us = types.KeyboardButton('О нас')
+    call_back = types.KeyboardButton('Обратный звонок')
+    order_event = types.KeyboardButton('Заказать мероприятие')
+    schedule = types.KeyboardButton( 'Расписание')
+    prices = types.KeyboardButton('Стоимость')
+    discount = types.KeyboardButton('Скидки')
+    who_registered = types.KeyboardButton('Кто записан')
+    to_register = types.KeyboardButton('Записаться на игру')
+    decline_register = types.KeyboardButton('Отменить запись')
+    vote_masters = types.KeyboardButton('Оценить ведущих')
+    masters_rating = types.KeyboardButton('Рейтинг ведущих')
+    members_rating = types.KeyboardButton('Рейтинг игроков')
+    markup.add(about_us, call_back, order_event, schedule, prices, discount, who_registered, to_register,
+               decline_register, vote_masters, masters_rating, members_rating)
+    return markup
+
+
+def master_buttons():
+    markup = types.ReplyKeyboardMarkup(row_width=3)
+    call_back = types.KeyboardButton('Обратный звонок')
+    order_event = types.KeyboardButton('Заказать мероприятие')
+    schedule = types.KeyboardButton( 'Расписание')
+    who_registered = types.KeyboardButton('Кто записан')
+    to_register = types.KeyboardButton('Записаться на игру')
+    decline_register = types.KeyboardButton('Отменить запись')
+    vote_masters = types.KeyboardButton('Оценить ведущих')
+    masters_rating = types.KeyboardButton('Рейтинг ведущих')
+    members_rating = types.KeyboardButton('Рейтинг игроков')
+    markup.add(call_back, order_event, schedule, who_registered, to_register,
                decline_register, vote_masters, masters_rating, members_rating)
     return markup
