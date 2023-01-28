8a79ee9fab84ab8c26a82000622928649156f82c
Ilya Tsibul <tsigel@vikivostok.ru>
2023-01-28 21:06:33 +0300

    Added weekday in Russian, changed order in standard commands in order to split 'em'

--- tgr_bot/schedule.py
+++ tgr_bot/schedule.py
@@ -1,13 +1,15 @@
 from game_management.models import Schedule
 import datetime
 from datetime import date, timedelta
+from backinfo import weekday_rus
 
 def schedule_text():
     date_start = date.today() - timedelta(days=date.today().weekday())
-    schedule = Schedule.objects.filter(date__gte=date_start, active=True).order_by('date')
+    schedule = Schedule.objects.filter(date__gte=date_start, active=True, date__lte=(date_start + timedelta(days=6))).\
+               order_by('date')
     text = ''
     for sch in schedule:
-        text += '*' + sch.date.strftime('%d\.%m') + ' ' + sch.date.strftime('%A') + '*\n' + \
+        text += '*' + sch.date.strftime('%d\.%m') + ' ' + weekday_rus(sch.date) + '*\n' + \
                 sch.schedule_time_begin.strftime('%H:%M') + ' \- ' + sch.schedule_time_end.strftime('%H:%M') + '\. ' + \
                 sch.game.game_name + '\. ' + sch.game.game_name + '\n' + \
                 sch.location.location_name + '\. ' + sch.location.location_address + '\.\n\n'
