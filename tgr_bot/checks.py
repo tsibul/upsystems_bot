5806d76933538681701f11b73d9f1d6d781eaac8
Ilya Tsibul <tsigel@vikivostok.ru>
2023-01-28 10:01:04 +0300

    check for click button for schedule, for main text, for prices, for discounts

--- tgr_bot/checks.py
+++ tgr_bot/checks.py
@@ -1,12 +1,30 @@
 from game_management.models import Member
+from backinfo import standard_commands as st
+from promo import promo_text, price_text, discount_text
+from schedule import schedule_text
 
 def hello_user(msg):
     member_id = msg.id
     try:
-        name = Member.objects.get(tg_id=member_id).nickname + ", рады новой встрече! \n" \
+        name = Member.objects.get(tg_id=member_id).nickname + ", рады новой встрече\! \n" \
                                                          "Чем хотите заняться?"
     except:
-        name = str(msg.username) + "!\nПривет! Первый раз здесь?\nЗалетай, регистрируйся =)\n" \
-                              "Записывайся на игры и не забывай оставлять комментарии."
+        name = str(msg.username) + "\!\n Первый раз здесь?\nЗалетай, регистрируйся \=\)\n" \
+                              "Записывайся на игры и не забывай оставлять комментарии\."
     return name
+
+
+def check_button(txt):
+    ind = st.index(txt)
+    if ind == 0:
+        text = promo_text()
+    elif ind == 3:
+        text = schedule_text()
+    elif ind == 4:
+        text = price_text()
+    elif ind == 5:
+        text = discount_text()
+    else:
+        text = 'under construction'
+    return text
 
