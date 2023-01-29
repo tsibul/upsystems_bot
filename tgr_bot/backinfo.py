cc89629303f0743a0111fb4fce20412df3c6d7d4
Ilya Tsibul <tsigel@vikivostok.ru>
2023-01-30 01:49:23 +0300

    split commands to 3 types Form 2 types of dictionaries for schedule (plan to use in register on games) replace special symbols in text

--- tgr_bot/backinfo.py
+++ tgr_bot/backinfo.py
@@ -1,32 +1,78 @@
-from datetime import datetime
+from datetime import date, timedelta
+from game_management.models import Schedule
+
 standard_commands = [
-                     'О нас',
-                     'Рейтинг ведущих',
-                     'Рейтинг игроков',
-                     'Расписание',
-                     'Стоимость',
-                     'Скидки',
-                     'Кто записан',
-                     'Записаться на игру',
-                     'Отменить запись',
-                     'Оценить ведущих',
-                     'Обратный звонок',
-                     'Заказать мероприятие',
-                     ]
+    'О нас',
+    'Рейтинг ведущих',
+    'Рейтинг игроков',
+    'Расписание',
+    'Стоимость',
+    'Скидки',
+    'Кто записан',
+]
+
+phone_no_commands = [
+    'Отменить запись',
+    'Оценить ведущих',
+    'Заказать мероприятие',
+]
+
+call_back_commands = [
+    'Обратный звонок',
+]
+
+order_event_commands = [
+    'Записаться на игру',
+]
+
 
 def weekday_rus(date):
     if date.weekday() == 0:
-        res = 'Понедельник'
+        res = 'ПОНЕДЕЛЬНИК'
     elif date.weekday() == 1:
-        res = 'Вторник'
+        res = 'ВТОРНИК'
     elif date.weekday() == 2:
-        res = 'Среда'
+        res = 'СРЕДА'
     elif date.weekday() == 3:
-        res = 'Четверг'
+        res = 'ЧЕТВЕРГ'
     elif date.weekday() == 4:
-        res = 'Пятница'
+        res = 'ПЯТНИЦА'
     elif date.weekday() == 5:
-        res = 'Суббота'
+        res = 'СУББОТА'
     else:
-        res = 'Воскресенье'
+        res = 'ВОСКРЕСЕНЬЕ'
     return res
+
+
+def promo_relpace(txt):
+    txt1 = txt.replace('.', '\.')
+    txt2 = txt1.replace('-', '\-')
+    txt3 = txt2.replace('/', '\/')
+    txt4 = txt3.replace('_', '\_')
+    return txt4
+
+def date_loc_sch_dict():
+    date_start = date.today() - timedelta(days=date.today().weekday())
+    schedule = Schedule.objects.filter(date__gte=date_start, active=True, date__lte=(date_start + timedelta(days=6))).\
+               order_by('date')
+    sch_dict = {}
+    for sch in schedule:
+        if sch.date not in sch_dict:
+            sch_dict[sch.date] = {}
+        if sch.location not in sch_dict[sch.date]:
+            sch_dict[sch.date][sch.location] = []
+        sch_dict[sch.date][sch.location].append(sch)
+    return sch_dict
+
+def date_gam_sch_dict():
+    date_start = date.today() - timedelta(days=date.today().weekday())
+    schedule = Schedule.objects.filter(date__gte=date_start, active=True, date__lte=(date_start + timedelta(days=6))).\
+               order_by('date')
+    sch_dict = {}
+    for sch in schedule:
+        if sch.date not in sch_dict:
+            sch_dict[sch.date] = {}
+        if sch.game not in sch_dict[sch.date]:
+            sch_dict[sch.date][sch.game] = []
+        sch_dict[sch.date][sch.game].append(sch)
+    return sch_dict
