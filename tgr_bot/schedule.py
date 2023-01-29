9b3c38d140aad50c9c3f726fd9f625dd92e7d94f
Ilya Tsibul <tsigel@vikivostok.ru>
2023-01-30 01:45:24 +0300

    two types of schedule date-game-content or date-location-content

--- tgr_bot/schedule.py
+++ tgr_bot/schedule.py
@@ -1,16 +1,34 @@
 from game_management.models import Schedule
 import datetime
 from datetime import date, timedelta
-from backinfo import weekday_rus
+from backinfo import weekday_rus, promo_relpace, date_loc_sch_dict, date_gam_sch_dict
 
-def schedule_text():
-    date_start = date.today() - timedelta(days=date.today().weekday())
-    schedule = Schedule.objects.filter(date__gte=date_start, active=True, date__lte=(date_start + timedelta(days=6))).\
-               order_by('date')
+
+def schedule_date_location():
+    sc = date_loc_sch_dict()
     text = ''
-    for sch in schedule:
-        text += '*' + sch.date.strftime('%d\.%m') + ' ' + weekday_rus(sch.date) + '*\n' + \
-                sch.schedule_time_begin.strftime('%H:%M') + ' \- ' + sch.schedule_time_end.strftime('%H:%M') + '\. ' + \
-                sch.game.game_name + '\. ' + sch.game.game_name + '\n' + \
-                sch.location.location_name + '\. ' + sch.location.location_address + '\.\n\n'
-    return text
+    for date in sc.keys():
+        text += '\n*' + weekday_rus(date) + ' ' + date.strftime('%d.%m') + '*\n'
+        for lc in sc[date].keys():
+            text += '*' + lc.location_name + '.* ' + lc.location_address + \
+                    '.\n' + lc.location_point + '\n'
+            for game in sc[date][lc]:
+                text += '*с ' + game.schedule_time_begin.strftime('%H:%M') + ' до ' + \
+                        game.schedule_time_end.strftime('%H:%M') + '*\n' + \
+                        game.game.game_name + '. ' + game.game.game_description + '\n'
+    return promo_relpace(text)
+
+
+def schedule_date_game():
+    sc = date_gam_sch_dict()
+    text = ''
+    for date in sc.keys():
+        text += '\n*' + weekday_rus(date) + ' ' + date.strftime('%d.%m') + '*\n'
+        for game in sc[date].keys():
+            text += 'Игры *' + game.game_name + '.* ' + game.game_description + '.\n'
+            for sch in sc[date][game]:
+                text += '*с ' + sch.schedule_time_begin.strftime('%H:%M') + ' до ' + \
+                        sch.schedule_time_end.strftime('%H:%M') + '*\n' + \
+                        sch.location.location_name + '. ' + sch.location.location_address + '\n' + \
+                        sch.location.location_point + '\n'
+    return promo_relpace(text)
