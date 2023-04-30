2b8cf928df61d7183f38e06c6a68bc9ac5e3872c
tsibul <grzegorzsc.bjencheshchykevic@gmail.com>
2023-04-30 13:54:55 +0300

    menu for register unregister and vote for master

--- tgr_bot/menu.py
+++ tgr_bot/menu.py
@@ -1,14 +1,18 @@
+import datetime
+from datetime import timedelta
+
 from telebot import types
+from backinfo import weekday_rus
+from game_management.models import Schedule, RegisteredToGame, Member
 
 
 def show_buttons(type):
     if type == 'игрок':
         return player_buttons()
-    elif type =='гость':
+    elif type == 'гость':
         return guest_buttons()
-    elif type =='ведущий':
+    elif type == 'ведущий':
         return master_buttons()
-
 
 
 def player_buttons():
@@ -16,7 +20,7 @@ def player_buttons():
     about_us = types.KeyboardButton('О нас')
     call_back = types.KeyboardButton('Обратный звонок')
     order_event = types.KeyboardButton('Заказать мероприятие')
-    schedule = types.KeyboardButton( 'Расписание')
+    schedule = types.KeyboardButton('Расписание')
     prices = types.KeyboardButton('Стоимость')
     discount = types.KeyboardButton('Скидки')
     who_registered = types.KeyboardButton('Кто записан')
@@ -29,12 +33,13 @@ def player_buttons():
                decline_register, vote_masters, masters_rating, members_rating)
     return markup
 
+
 def guest_buttons():
     markup = types.ReplyKeyboardMarkup(row_width=3)
     about_us = types.KeyboardButton('О нас')
     call_back = types.KeyboardButton('Обратный звонок')
     order_event = types.KeyboardButton('Заказать мероприятие')
-    schedule = types.KeyboardButton( 'Расписание')
+    schedule = types.KeyboardButton('Расписание')
     prices = types.KeyboardButton('Стоимость')
     discount = types.KeyboardButton('Скидки')
     who_registered = types.KeyboardButton('Кто записан')
@@ -52,7 +57,7 @@ def master_buttons():
     markup = types.ReplyKeyboardMarkup(row_width=3)
     call_back = types.KeyboardButton('Обратный звонок')
     order_event = types.KeyboardButton('Заказать мероприятие')
-    schedule = types.KeyboardButton( 'Расписание')
+    schedule = types.KeyboardButton('Расписание')
     who_registered = types.KeyboardButton('Кто записан')
     to_register = types.KeyboardButton('Записаться на игру')
     decline_register = types.KeyboardButton('Отменить запись')
@@ -92,4 +97,28 @@ def member_register():
     discard = types.KeyboardButton('Отмена')
     markup.row(nickname, photo, birth_date)
     markup.row(discard)
+    return markup
+
+
+def game_register_menu_date(lead_id, register_type):
+    today = datetime.date.today()
+    markup = types.ReplyKeyboardMarkup()
+    if register_type == 'checkin_game':
+        games_available = Schedule.objects.filter(date__gte=today, date__lte=today + timedelta(days=6)).values_list(
+            'date', flat=True).distinct().order_by('date')
+        for game in games_available:
+            msg_text = weekday_rus(game) + ' ' + game.strftime('%d.%m.%Y') + ' ' + 'Записаться'
+            markup.row(msg_text)
+    elif register_type == 'checkout_game':
+        games_registered = RegisteredToGame.objects.filter(member__lead__tg_id=lead_id, schedule__date__gte=today,
+                                                           schedule__date__lte=today + timedelta(days=6)).values_list(
+            'schedule__date', flat=True).distinct().order_by('schedule__date')
+        for game in games_registered:
+            msg_text = weekday_rus(game) + ' ' + game.strftime('%d.%m.%Y') + ' Записаться'
+            markup.row(msg_text)
+    elif register_type == 'master_rating':
+        game_masters = Member.objects.filter(game_function__game_function='ведущий')
+        for master in game_masters:
+            markup.row(master + ' Оценить')
+    markup.row('Отмена')
     return markup
